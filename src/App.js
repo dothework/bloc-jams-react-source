014f40993b95958148249cd7d5b0bf22a70b6d10
dothework <marktc@gmail.com>
2017-11-13 08:44:16 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import './App.css';
+import Landing from './components/Landing';
+import Library from './components/Library';
+import Album from './components/Album';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <nav>
+            <Link to='/'><img src="./../assets/images/bloc_jams_logo.png" alt="Bloc Jams Landing" /></Link>
+            <Link to='/library'>Library</Link>
+          </nav>
+        </header>
+        <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
+          <Route path="/album/:slug" component={Album} />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/data/albums.js
@@ -0,0 +1,29 @@
+export default [{
+  title: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  slug: 'pablo-picasso-the-colors',
+  albumCover: '/assets/images/album_covers/01.jpg',
+  songs: [
+      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+  ]
+}, {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    slug: 'guglielmo-marconi-the-telephone',
+    albumCover: '/assets/images/album_covers/02.jpg',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21' },
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15' }
+    ]
+}];
